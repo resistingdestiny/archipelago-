6888ddc0bb4b8de8730ff2cbe7ebe8aa46a6b629
Lucas <lucas.schneider96@web.de>
2023-11-18 11:26:45 +0000

    adding commit function, view policies, pricing

--- contracts/insurancePolicy.sol
+++ contracts/insurancePolicy.sol
@@ -1,44 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IUniswapV2Router {
-    // Define interface methods to interact with Uniswap for pricing
+interface IUniswapV3Pool {
+    function slot0() external view returns (
+        uint160 sqrtPriceX96,
+        int24 tick,
+        uint16 observationIndex,
+        uint16 observationCardinality,
+        uint16 observationCardinalityNext,
+        uint8 feeProtocol,
+        bool unlocked
+    );
 }
 
 contract InsurancePolicyContract {
-    struct InsurancePolicy {
-        uint256 limit;
-        string region;
+    struct Location {
         uint256 longitude;
         uint256 latitude;
         uint256 radius;
-        uint256 coverPeriodStart;
-        uint256 coverPeriodEnd;
+    }
+
+    struct Period {
+        uint256 start;
+        uint256 end;
+    }
+
+    struct InsurancePolicy {
+        uint256 limit;
+        string region;
+        Location location;
+        Period coverPeriod;
         uint256 probability;
         string poolType;
         bool requestFundFromUNICEF;
         uint256 premium;
         uint256 fundsCommitted;
-        address denomination; // Denomination token (ETH, BTC, USDC)
+        address denomination;
         address[] acceptedTokens;
+        address creator;
+        mapping(address => uint256) userFundsCommitted; // Track funds committed per user
     }
 
     mapping(uint256 => InsurancePolicy) public insurancePolicies;
     uint256 public nextPolicyId;
-    IUniswapV2Router public uniswapRouter;
+    IUniswapV3Pool public uniswapPool;
 
-    constructor(address _uniswapRouter) {
-        uniswapRouter = IUniswapV2Router(_uniswapRouter);
+    constructor(address _uniswapPool) {
+        uniswapPool = IUniswapV3Pool(_uniswapPool);
     }
 
     function createInsurancePolicy(
         uint256 _limit,
         string memory _region,
-        uint256 _longitude,
-        uint256 _latitude,
-        uint256 _radius,
-        uint256 _coverPeriodStart,
-        uint256 _coverPeriodEnd,
+        Location memory _location,
+        Period memory _coverPeriod,
         uint256 _probability,
         string memory _poolType,
         bool _requestFundFromUNICEF,
@@ -46,37 +61,36 @@ contract InsurancePolicyContract {
         address _denomination,
         address[] memory _acceptedTokens
     ) public {
-        require(_coverPeriodEnd > _coverPeriodStart, "Invalid cover period");
+        require(_coverPeriod.end > _coverPeriod.start, "Invalid cover period");
         
         InsurancePolicy storage policy = insurancePolicies[nextPolicyId++];
         policy.limit = _limit;
         policy.region = _region;
-        policy.longitude = _longitude;
-        policy.latitude = _latitude;
-        policy.radius = _radius;
-        policy.coverPeriodStart = _coverPeriodStart;
-        policy.coverPeriodEnd = _coverPeriodEnd;
+        policy.location = _location;
+        policy.coverPeriod = _coverPeriod;
         policy.probability = _probability;
         policy.poolType = _poolType;
         policy.requestFundFromUNICEF = _requestFundFromUNICEF;
         policy.premium = _premium;
         policy.denomination = _denomination;
         policy.acceptedTokens = _acceptedTokens;
+        policy.creator = msg.sender;
     }
 
     function commitFunds(uint256 policyId, address token, uint256 amount) public {
         require(policyId < nextPolicyId, "Invalid policy ID");
         InsurancePolicy storage policy = insurancePolicies[policyId];
-        require(policy.fundsCommitted < policy.limit, "Limit reached");
+        require(policy.fundsCommitted + amount <= policy.limit, "Limit exceeded");
 
         uint256 tokenValueInDenomination = getTokenValueInDenomination(token, amount, policy.denomination);
         policy.fundsCommitted += tokenValueInDenomination;
+        policy.userFundsCommitted[msg.sender] += tokenValueInDenomination; // Track user's committed funds
     }
 
     function getTokenValueInDenomination(address token, uint256 amount, address denomination) private view returns (uint256) {
-        // Implement logic to get token value in the specified denomination using Uniswap
-        // This is a placeholder for the actual implementation
-        return 0;
+        (uint160 sqrtPriceX96,,,,,,) = uniswapPool.slot0();
+        uint256 price = uint256(sqrtPriceX96) * uint256(sqrtPriceX96) >> 96;
+        return amount * price;
     }
 
     function getAllPolicies() public view returns (InsurancePolicy[] memory) {
@@ -87,5 +101,4 @@ contract InsurancePolicyContract {
         return policies;
     }
 
-    // ... [Other functions]
 }
